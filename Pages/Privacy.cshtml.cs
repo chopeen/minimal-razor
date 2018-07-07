a2e71aec1537bc803707a1eded2013a1c6d8eed4
chopeen <chopeen@gmail.com>
2018-07-07 23:45:35 +0200

    initial import (`dotnet new razor`);

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace last_fm_not_mine_alert_razor.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
