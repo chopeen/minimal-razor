e0a1f15ba50ee61f4e9e178e8881ef73ff946615
chopeen <chopeen@gmail.com>
2018-08-22 21:56:41 +0200

    new page to add configuration through DI;

---
+++ Pages/PageWithConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace minimal_razor.Pages
+{
+    public class PageWithConfig : PageModel
+    {
+
+    }
+}
