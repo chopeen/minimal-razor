22af1b9fea49355c3115454eaecdcb35613ae5cf
chopeen <chopeen@gmail.com>
2018-08-22 22:48:40 +0200

    logger injected into the model;

--- Pages/PageWithConfig.cs
+++ Pages/PageWithConfig.cs
@@ -5,17 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace minimal_razor.Pages
 {
     public class PageWithConfig : PageModel
     {
-        // injecting the configuration into the model
+        // injecting the configuration and logger into the model
         public readonly IConfiguration Configuration;
+        public readonly ILogger<PageWithConfig> Logger;
 
-        public PageWithConfig(IConfiguration configuration)
+        public PageWithConfig(IConfiguration configuration, ILogger<PageWithConfig> logger)
         {
             this.Configuration = configuration;
+            this.Logger = logger;
         }
     }
 }
