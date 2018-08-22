bb9df4974fb3cedf1f38d01b65edae3f3bb9a106
chopeen <chopeen@gmail.com>
2018-08-22 22:24:22 +0200

    configuration injected into the model;

--- Pages/PageWithConfig.cs
+++ Pages/PageWithConfig.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace minimal_razor.Pages
 {
     public class PageWithConfig : PageModel
     {
+        // injecting the configuration into the model
+        public readonly IConfiguration Configuration;
 
+        public PageWithConfig(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
     }
 }
